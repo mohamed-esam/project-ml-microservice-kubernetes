3182f29b64ff2cafb472b32adb5b665eab3385c1
“mohamed-esam” <mohamed-esam79@outlook.com>
2020-09-24 02:08:57 +0000

    run kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mesam95/mesam95:dockerproj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run dockerproj4 --image=$dockerpath --port=8001
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward dockerproj4 8001:80
